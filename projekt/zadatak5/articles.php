57cba486114e2b451cfb4232d96f07ef6ba039df
kristinaa14 <kristina.anicic1@gmail.com>
2024-11-08 02:06:52 +0100

    Added articles.php and images

---
+++ projekt/zadatak5/articles.php
@@ -0,0 +1,266 @@
+<?php
+    if ($article == 1) {
+        print '
+        <div class="container article">
+            <h2>The Evolution of RPGs: From Classic Adventures to Open Worlds</h2>
+            <h4>Tracing the Journey of Role-Playing Games from Text-Based Origins to Immersive Worlds</h4>
+            </br>
+            <div class="row">
+                <div class="img_news col-md-4 col-lg-3 mb-2">
+                    <figure>
+                        <img src="img/news/news-article-1.jpg" alt="RPG-game" title="RPG-game"/>
+                        <figcaption>Award winning action-RPG ELDEN RING sold 20 million units worldwide.</figcaption>
+                    </figure>
+                </div>
+                <div class="img_news col-md-4 col-lg-3 mb-2">
+                    <figure>
+                        <img src="img/news/news-article-1-2.jpg" alt="" title=""/>
+                        <figcaption>Baldur\'s Gate 3 is a story-driven, party RPG set in the Dungeons & Dragons universe, where your choices influence a tale of fellowship and betrayal.</figcaption>
+                    </figure>
+                </div>
+                <div class="img_news col-md-4 col-lg-3 mb-2">
+                    <figure>
+                        <img src="img/news/news-article-1-3.jpg" alt="" title=""/>
+                        <figcaption>Cyberpunk is a tabletop role-playing game in the dystopian science fiction genre, written by Mike Pondsmith</figcaption>
+                    </figure>
+                </div>
+            </div>
+            </br>
+            <div style="clear:both;"></div>
+            <p>Early RPGs were simple text-based adventures that allowed players to explore worlds through the power of their 
+            imagination. Titles like Zork and Colossal Cave Adventure offered minimal graphics but stood out for their rich 
+            stories and challenging puzzles, giving players a strong sense of immersion.</p>
+            
+            <p>Over time, RPGs evolved into graphically sophisticated games like Ultima and Wizardry, introducing 2D graphics 
+            and more complex combat systems. During the \'80s and \'90s, console RPGs like Final Fantasy and Chrono Trigger 
+            won players’ hearts worldwide, further raising expectations within the RPG genre.</p>
+            
+            <p>The introduction of 3D graphics and open worlds in titles like The Elder Scrolls and Baldur’s Gate allowed 
+            players freer exploration and nonlinear storytelling, along with more character customization options. RPGs became 
+            deeper and more varied, allowing players to shape their adventures.</p>
+            
+            <p>MMORPGs like World of Warcraft opened doors for shared adventures and team missions with millions of players 
+            globally, bringing a new social aspect to RPGs and widening possibilities for cooperative exploration.</p>
+
+            <p>Today, advancements in virtual reality and artificial intelligence bring new possibilities to the RPG genre. 
+            Games now use technology to offer personalized stories and ever-evolving worlds. The future of RPGs promises even 
+            deeper storytelling and more innovative ways to craft personal adventures.</p>
+            <time datetime="2024-11-01">01 September 2024</time>
+            <hr>
+            <a href="index.php?menu='.$menu.'">Back</a>
+        
+        </div>';
+    }
+    else if ($article == 2) {
+        print '
+        <div class="container article">
+            <h2>Top Indie Games of 2024: Hidden Gems Worth Playing</h2>
+            <h4>Celebrating Creative Freedom and Unique Experiences in Indie Gaming</h4>
+            </br>
+            <div class="row">
+                <div class="img_news col-md-4 col-lg-3 mb-2">
+                    <figure>
+                        <img src="img/news/news-article-3.jpg" alt="" title=""/>
+                        <figcaption>Tunic is a 2022 action-adventure game developed by Isometricorp Games and published by Finji.</figcaption>
+                    </figure>
+                </div>
+                <div class="img_news col-md-4 col-lg-3 mb-2">
+                    <figure>
+                        <img src="img/news/news-article-3-2.jpg" alt="" title=""/>
+                        <figcaption>Sea of Stars has won Best Indie Game at the Golden Joystick Awards 2023 powered by Intel.</figcaption>
+                    </figure>    
+                </div>
+                <div class="img_news col-md-4 col-lg-3 mb-2">
+                    <figure>
+                        <img src="img/news/news-article-3-3.jpg" alt="" title=""/>
+                        <figcaption>Death’s Door is a charming boss battle dungeon crawler with challenging rather than brutal combat.</figcaption>
+                    </figure>    
+                </div>
+            </div>
+            </br>
+            <div style="clear:both;"></div>
+            <p>Indie games often bring unique creativity and freedom that AAA titles lack. Independent developers can 
+            experiment and explore stories, styles, and gameplay without the constraints of major publishers, making 
+            the indie scene a source of diversity and originality.</p>
+            
+            <p>In 2024, the indie scene offers a wide array of innovative titles. Games like Celeste and Hollow Knight have 
+            set the standard for quality and creativity in indie gaming, while new titles bring fresh ideas and approaches.</p>
+            
+            <p>Indie games often attract players with retro styles and pixel-art graphics that evoke nostalgia, while 
+            also bringing modern depth and challenge. With minimalist aesthetics, these games often offer complex stories 
+            and challenging gameplay that appeal to a broad audience.</p>
+
+            <p>Big hits like Hades and Undertale show how indie games can exceed expectations and become global 
+            phenomena. Many indie developers work alone or in small teams, creating personal, auteur-driven games 
+            that frequently tackle important themes and social issues.</p>
+
+            <p>Indie games will continue to be a vital part of the gaming industry, bringing authenticity and innovation. 
+            2024 promises even more unforgettable indie titles that will continue to inspire players and set new standards 
+            in the world of video games.</p>
+
+            <time datetime="2024-10-15">15 October 2024</time>
+            <hr>
+            <a href="index.php?menu='.$menu.'">Back</a>
+        
+        </div>';
+    }
+
+    else if ($article == 3) {
+        print '
+        <div class="container article">
+            <h2>Esports in 2024: The Rise of New Contenders</h2>
+            <h4>Exploring the Rapid Expansion and New Talent in Competitive Gaming<h4>
+            </br>
+            <div class="row">
+                <div class="img_news col-md-4 col-lg-3 mb-2">
+                    <figure>
+                        <img src="img/news/news-article-2.jpg" alt="" title=""/>
+                        <figcaption>Players competing in a League of Legends tournament</figcaption>
+                    </figure>    
+                </div>
+                <div class="img_news col-md-4 col-lg-3 mb-2">
+                    <figure>
+                        <img src="img/news/news-article-2-3.jpg" alt="" title=""/>
+                        <figcaption>G2 Esports won the 2023 LEC Season Finals with a 3–1 victory over Fnatic</figcaption>
+                    </figure>    
+                </div>
+            </div>
+            </br>
+            <div style="clear:both;"></div>          
+            <p>The esports industry has grown immensely, evolving from small competitions in the early 2000s to a global 
+            industry that includes millions of viewers and professional players. Tournaments like the League of Legends 
+            World Championship and The International have become massive events, with prize pools that surpass many 
+            traditional sports.</p>
+            
+            <p>The year 2024 brings fresh faces and new titles to the esports scene, from innovative games like Valorant and Apex 
+            Legends to mobile games popular in Asia, such as PUBG Mobile and Free Fire. This variety of games broadens the pool 
+            of players and teams competing at a high level.</p>
+
+            <p>The rise of mobile esports has opened doors for young talent from countries where mobile platforms are more 
+            popular than consoles. In many parts of the world, mobile esports has become a serious platform, attracting teams, 
+            sponsors, and growing viewership.</p>
+
+            <p>Support from major brands and sponsors has helped esports reach new heights. Companies like Red Bull, Coca-Cola, 
+            and tech giants are investing in esports, increasing the industry’s visibility and allowing new events and leagues 
+            to emerge.<p>
+            
+            <p>In the future, esports could become part of the Olympic program, further expanding the reach and legitimacy of 
+            this industry. With new technologies like virtual and augmented reality, the esports scene is becoming richer, 
+            offering more opportunities for professional players and viewers worldwide.</p>
+
+            <time datetime="2024-10-28">28 October 2024</time>
+            <hr>
+            <a href="index.php?menu='.$menu.'">Back</a>
+        
+        </div>';
+    }
+
+
+    else if ($article == 4) {
+        print '
+        <div class="container article">
+            <h2>The Future of Gaming Graphics: Ray Tracing and Beyond</h2>
+            <h4>How Emerging Graphics Technology is Transforming Visual Realism in Games</h4>
+            </br>
+            <div class="row">
+                <div class="img_news col-md-4 col-lg-3 mb-2">
+                    <figure>
+                        <img src="img/news/news-article-4.jpg" alt="" title=""/>
+                        <figcaption>Deathloop supports Ray Tracing Sun Shadows and Ray Tracing Ambient Occlusion.</figcaption>
+                    </figure>
+                </div>
+                <div class="img_news col-md-4 col-lg-3 mb-2">
+                    <figure>
+                        <img src="img/news/news-article-4-2.png" alt="" title=""/>
+                        <figcaption>n these screenshots from Cyberpunk 2077, you can clearly see the difference when ray tracing is turned on and off.</figcaption>
+                    </figure>
+                </div>
+                <div class="img_news col-md-4 col-lg-3 mb-2">
+                    <figure>
+                        <img src="img/news/news-article-4-3.jpg" alt="" title=""/>
+                        <figcaption>Microsoft’s Mojang and Nvidia are adding a rendering technique called ray tracing to “Minecraft.”</figcaption>
+                    </figure>
+                </div>
+            </div>
+            </br>
+            <div style="clear:both;"></div>
+            <p>Ray tracing brings a new level of realism to games by simulating natural light, shadows, and reflections in a 
+            way that was previously possible only in movies. Games like Cyberpunk 2077 and Control use this technology to 
+            create stunning worlds, setting new expectations for visuals in gaming.</p>
+            
+            <p>Ray tracing allows for incredible visual depth, especially in scenes with reflective surfaces or complex light 
+            sources. With faster GPUs and better development tools, ray tracing is now accessible even in mainstream titles.</p>
+
+            <p>Besides ray tracing, new technologies like DLSS (Deep Learning Super Sampling) enable games to maintain 
+            high resolution and performance using artificial intelligence. DLSS allows for smoother graphics with fewer 
+            resources, making it a crucial tool for game development.</p>
+
+            <p>Virtual reality (VR) introduces new possibilities in graphics. With VR technology, players can step into 
+            fully realistic 3D worlds, which requires impressive graphics that can keep up with all player movements 
+            and reactions in real-time.</p>
+
+            <p>The future of graphics technology in gaming promises stunning visuals that will create a deeper gaming 
+            experience. As hardware continues to improve, games are expected to look increasingly realistic, 
+            allowing players to immerse themselves in worlds that feel almost lifelike.</p>
+            
+            <time datetime="2024-11-03">03 September 2024</time>
+            <hr>
+            <a href="index.php?menu='.$menu.'">Back</a>
+        
+        </div>';
+    }
+
+    else if ($article == 5) {
+        print '
+        <div class="container article">
+            <h2>Mobile Gaming Revolution: How Smartphones Became Gaming Consoles</h2>
+            <h4>The Rise of Mobile Gaming as a Serious Platform for Immersive Experiences</h4>
+            </br>
+            <div class="row">
+                <div class="img_news col-md-4 col-lg-3 mb-2">
+                    <figure>
+                        <img src="img/news/news-article-5.jpg" alt="" title=""/>
+                        <figcaption>Mobile gaming is the most popular form of gaming in the world, overtaking both console and PC gaming. </figcaption>
+                    </figure>
+                </div>
+                <div class="img_news col-md-4 col-lg-3 mb-2">
+                    <figure>
+                        <img src="img/news/news-article-5-2.jpg" alt="" title=""/>
+                        <figcaption>At the gamescom award 2024, Genshin Impact has won the "Best Mobile Game" award</figcaption>
+                    </figure>
+                </div>
+                <div class="img_news col-md-4 col-lg-3 mb-2">
+                    <figure>
+                        <img src="img/news/news-article-5-3.jpg" alt="" title=""/>
+                        <figcaption>Arm\'s Open-Source Upscaler Brings PC-Quality Graphics to Smartphones</figcaption>
+                    </figure>
+                </div>
+            </div>
+            </br>
+            <div style="clear:both;"></div>
+            <p>Mobile gaming has come a long way from simple titles like Snake and Tetris to graphically advanced games 
+            that rival console titles today. With advancements in hardware and software, mobile gaming has become a 
+            significant part of the gaming industry.</p>
+
+            <p>Titles like Genshin Impact and Call of Duty: Mobile have redefined mobile gaming, offering players fully 
+            immersive experiences even on smartphones. These games use advanced graphics and complex combat systems that 
+            were once impossible on mobile platforms.
+            In many parts of the world, especially in Asia, mobile games have become the dominant platform for gamers. 
+            Many games are designed specifically for mobile devices, using customized controls and optimized performance 
+            for on-the-go gaming.</p>
+
+            <p>Smartphone manufacturers now actively optimize devices for gamers, adding faster processors, 
+            enhanced displays, and features like cooling systems to allow for uninterrupted gameplay. These devices 
+            are increasingly similar to portable consoles.
+            Mobile gaming will continue to transform the gaming industry, and smartphones will keep evolving to support 
+            ever more demanding games. The future of mobile gaming promises even richer and more engaging experiences as 
+            technology advances.</p>
+            
+            <time datetime="2024-10-22">22 October 2024</time>
+            <hr>
+            <a href="index.php?menu='.$menu.'">Back</a>
+        
+        </div>';
+    }
+
+?>
