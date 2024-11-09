803b4702030210bff85f4a95feb3b52eab8f5d0d
kristinaa14 <kristina.anicic1@gmail.com>
2024-11-09 06:30:45 +0100

    added about-us.php

---
+++ projekt/zadatak5/about-us.php
@@ -0,0 +1,15 @@
+<?php
+print'
+    <div class="container about-us">
+        <h2>Who we are</h2>
+        <video width="45%" controls poster="img/video-thumbnail.jpg" >
+        <source src="video/The Gaming Industry _ Start Here.mp4" type="video/mp4">
+        Your browser does not support HTML5 video.
+        </video>
+        <p>Welcome to our website, a space born out of pure passion for gaming! Our journey began with a small group of friends, each with a shared love for video games and a vision to create a platform where gamers of all kinds could come together. </p>
+        <p>Our mission is to offer not only a comprehensive gallery of game titles across all genres but also a space for engaging stories and updates from the gaming universe. We carefully select each game in our collection, ensuring something for every type of player, from casual gamers to those looking for deep, immersive experiences.</p>
+        <p>Alongside our game gallery, our news section keeps you informed on the latest releases, exclusive previews, community updates, and game reviews, so you’re always up-to-date with what’s happening in the gaming world. Our site continues to grow and evolve with the support of the gaming community, and we’re constantly exploring ways to improve your experience.</p>
+        <p>Thank you for joining us on this journey. Whether you’re here to discover new games, stay informed on the latest news, or connect with other gamers, we hope our website becomes your go-to hub for everything gaming. Let’s explore and celebrate this incredible world of games together!</p>
+    </div>
+    ';
+?>
