70d3c8d2058e87e2446f27eba171d61f3d189033
kristinaa14 <kristina.anicic1@gmail.com>
2024-11-19 21:24:34 +0100

    improved navbar responsiveness with hamburger menu

--- projekt/konacni_projekt/index.php
+++ projekt/konacni_projekt/index.php
@@ -14,8 +14,7 @@ print'
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="icon" href="img/favicon.ico" type="image/x-icon>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+        <link rel="icon" href="img/favicon.ico" type="image/x-icon> 
         <title>GameSphere</title>
     </head>
     <body>';
@@ -24,9 +23,14 @@ print'
         if(isset($_GET['action'])) { $action   = (int)$_GET['action'] ; }
 
         print'
-        <nav class="navbar navbar-expand-lg">';
-            include("menu.php");
+        <nav class="navbar navbar-expand-lg">
+            <div class="container navbar-expand-lg">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-navicon" style="color:#fff; font-size:28px; padding: 10px"></i>
+                </button>';
+                include("menu.php");
         print '
+            </div>
         </nav>
         '; 
 
@@ -125,6 +129,7 @@ print'
                 </p>
             </div>
         </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
     </body>
 </html>';
 ?>
--- projekt/konacni_projekt/menu.php
+++ projekt/konacni_projekt/menu.php
@@ -1,48 +1,46 @@
 <?php
     print '
-    <div class="container">
-        <div class="navbar-collapse justify-content-center">
-            <ul class="navbar-nav">
+    <div class="collapse navbar-collapse justify-content-center navbar-expand-lg" id="navbarNav">
+        <ul class="navbar-nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?menu=1">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?menu=2">News</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?menu=3">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?menu=4">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?menu=5">Gallery</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?menu=10">Games API</a>
+            </li>';
+            if (!isset($_SESSION['user']['valid']) || $_SESSION['user']['valid'] == 'false') {
+                print '
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?menu=1">Home</a>
+                    <a class="nav-link" href="index.php?menu=6">Register</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?menu=2">News</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?menu=3">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?menu=4">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?menu=5">Gallery</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?menu=10">Games API</a>
+                    <a class="nav-link" href="index.php?menu=7">Sign in</a>
                 </li>';
-                if (!isset($_SESSION['user']['valid']) || $_SESSION['user']['valid'] == 'false') {
-                    print '
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?menu=6">Register</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?menu=7">Sign in</a>
-                    </li>';
-                }
+            }
 
-                else if ($_SESSION['user']['valid'] == 'true') {
-                    print '
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?menu=8">Admin</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?menu=9">Sign out</a>
-                    </li>';
-                }
-            print'  
-            </ul>
-        </div>
+            else if ($_SESSION['user']['valid'] == 'true') {
+                print '
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?menu=8">Admin</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?menu=9">Sign out</a>
+                </li>';
+            }
+        print'  
+        </ul>
     </div>
     ';
 
